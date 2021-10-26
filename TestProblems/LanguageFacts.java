45660e694701d15704ffd81c484c4f6b3d62e214
Kory Attleson <kory.attleson@gmail.com>
2021-10-26 11:34:15 -0500

    Commented out old code problems. Added new code to print lines in console.

---
+++ TestProblems/LanguageFacts.java
@@ -0,0 +1,41 @@
+package TestProblems;
+
+/*
+class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello Word!");
+    }
+}
+*/
+
+/*
+public class HideAndSeek {
+    public static void main(String[] args) {
+      System.out.println("Let's play hide and seek.");
+      System.out.print("Three...");
+      System.out.print("Two...");
+      System.out.println("One...");
+      System.out.println("Ready or not, here I come!");
+  
+    }
+  }
+  */
+
+/*
+  public class LanguageFacts {
+    public static void main(String[] args) {
+      // Press enter or return on your keyboard after each semicolon!
+      
+      System.out.println("Java is a class-based language.");
+  
+      System.out.println("Java classes have a 'main' method.");
+      
+      System.out.println("Java statements end with a semicolon.");
+  
+      System.out.println("Programming is difficult but rewarding");
+    }
+  }
+
+  */
+
+  
--- TestProblems/TestProblems.java
+++
@@ -1,7 +0,0 @@
-package TestProblems;
-
-class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello Word!");
-    }
-}
